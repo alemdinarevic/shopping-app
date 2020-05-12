6f7b31ccd4a0dde03e254768376a9aabb7993653
Alem Dinarević <alemdinarevic@gmail.com>
2020-05-12 21:23:08 +0200

    added items, details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,10 +10,14 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe ('Test recipe', 'Delicios test', 'https://dish.co.nz/media/VERSIONS/images/ceviche-citrus_article--740x1005.jpg')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
